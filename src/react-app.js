8c4e7849576f5092d7cb11db57950392d324d34d
Kent C. Dodds <me+github@kentcdodds.com>
2020-03-28 15:33:29 -0600

    feat: add class names

--- src/react-app.js
+++ src/react-app.js
@@ -413,7 +413,10 @@ function renderReactApp({
                           </div>
                         }
                       >
-                        <div css={mq({paddingBottom: [0, 0, '2rem']})}>
+                        <div
+                          className="exercise-container"
+                          css={mq({paddingBottom: [0, 0, '2rem']})}
+                        >
                           {exerciseElement}
                         </div>
                       </React.Suspense>
@@ -468,7 +471,10 @@ function renderReactApp({
                           </div>
                         }
                       >
-                        <div css={mq({paddingBottom: [0, 0, '2rem']})}>
+                        <div
+                          className="final-container"
+                          css={mq({paddingBottom: [0, 0, '2rem']})}
+                        >
                           {finalElement}
                         </div>
                       </React.Suspense>
@@ -981,61 +987,63 @@ function renderReactApp({
     const theme = getTheme(mode)
 
     return (
-      <ThemeProvider theme={theme}>
-        <Router history={history}>
-          <Switch>
-            <Route exact path="/">
-              <Home mode={mode} setMode={setMode} />
-            </Route>
-            <Route exact path="/:exerciseNumber">
-              <ExerciseContainer mode={mode} setMode={setMode} />
-            </Route>
-            <Route>
-              <NotFound />
-            </Route>
-          </Switch>
-        </Router>
-        <Global
-          styles={{
-            'html, body, #root': {
-              background: theme.background,
-              color: theme.text,
-            },
-            '::selection': {
-              background: theme.primary,
-              color: 'white',
-            },
-            '[data-reach-tab]': {
-              cursor: 'pointer',
-            },
-            a: {
-              color: theme.primary,
-            },
-            /*
+      <div className="react-workshop-app">
+        <ThemeProvider theme={theme}>
+          <Router history={history}>
+            <Switch>
+              <Route exact path="/">
+                <Home mode={mode} setMode={setMode} />
+              </Route>
+              <Route exact path="/:exerciseNumber">
+                <ExerciseContainer mode={mode} setMode={setMode} />
+              </Route>
+              <Route>
+                <NotFound />
+              </Route>
+            </Switch>
+          </Router>
+          <Global
+            styles={{
+              'html, body, #root': {
+                background: theme.background,
+                color: theme.text,
+              },
+              '::selection': {
+                background: theme.primary,
+                color: 'white',
+              },
+              '[data-reach-tab]': {
+                cursor: 'pointer',
+              },
+              a: {
+                color: theme.primary,
+              },
+              /*
               This will hide the focus indicator if the element receives focus via the mouse,
               but it will still show up on keyboard focus.
             */
-            '.js-focus-visible :focus:not(.focus-visible)': {
-              outline: 'none',
-            },
-            hr: {
-              opacity: 0.5,
-              border: 'none',
-              height: 1,
-              background: theme.textLightest,
-              maxWidth: '100%',
-              marginTop: 30,
-              marginBottom: 30,
-            },
-          }}
-        />
-        <Global
-          styles={`
+              '.js-focus-visible :focus:not(.focus-visible)': {
+                outline: 'none',
+              },
+              hr: {
+                opacity: 0.5,
+                border: 'none',
+                height: 1,
+                background: theme.textLightest,
+                maxWidth: '100%',
+                marginTop: 30,
+                marginBottom: 30,
+              },
+            }}
+          />
+          <Global
+            styles={`
               ${mode === 'light' ? prismThemeLight : prismThemeDark}
             `}
-        />
-        <DelayedTransition />
-      </ThemeProvider>
+          />
+          <DelayedTransition />
+        </ThemeProvider>
+      </div>
     )
   }
 
