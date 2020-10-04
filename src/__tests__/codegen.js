0548ebfa3379cc1519f9ba28d896abf9b103ac1e
Markus Lasermann <github@ml74.de>
2020-10-04 13:56:56 +0200

    Make test (more) platform independent #18

--- src/__tests__/codegen.js
+++ src/__tests__/codegen.js
@@ -1,13 +1,19 @@
 import path from 'path'
 import codegen from '../codegen'
 
-const cwd = path.join(__dirname, '../../example/react-fundamentals')
+const cwd = path
+  .join(__dirname, '../../example/react-fundamentals')
+  .split(path.sep)
+  .join('/')
 
 expect.addSnapshotSerializer({
   test(val) {
     return typeof val === 'string'
   },
   print(val) {
+    if (process.platform === 'win32') {
+      val = val.replace(/\\\\/g, '/')
+    }
     return val.split(cwd).join('<PROJECT_ROOT>')
   },
 })
