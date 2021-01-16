2d2910ef3a1cd4415778fda075cb7f86c9261b2f
Kent C. Dodds <me+github@kentcdodds.com>
2021-01-16 14:14:44 -0700

    fix a bunch of things

--- codegen.js
+++ codegen.js
@@ -1,1 +1,6 @@
-module.exports = require('./dist/codegen').getCode
+if (process.cwd() === __dirname) {
+  // we're running local tests in this case, so don't use dist
+  module.exports = require('./src/codegen').getCode
+} else {
+  module.exports = require('./dist/codegen').getCode
+}
--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-module.exports = require('./dist/server')
+if (process.cwd() === __dirname) {
+  // we're running local tests in this case, so don't use dist
+  module.exports = require('./src/server')
+} else {
+  module.exports = require('./dist/server')
+}
